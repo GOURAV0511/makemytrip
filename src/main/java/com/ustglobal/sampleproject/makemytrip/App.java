c9c953d834b74ee91a62200eff87afa0203d5c07
151468 <151468@ust-global.com>
2020-10-23 14:47:24 +0530

    initial commit

---
+++ src/main/java/com/ustglobal/sampleproject/makemytrip/App.java
@@ -0,0 +1,13 @@
+package com.ustglobal.sampleproject.makemytrip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
